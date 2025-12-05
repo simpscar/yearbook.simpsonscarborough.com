fddf44fb0b7df95cf38fc0d10d154c8150fbaa40
Ben Loeffler <b@ben.direct>
2025-12-05 13:25:58 -0800

    Add read:org scope to OAuth request

--- api/auth.js
+++ api/auth.js
@@ -5,7 +5,8 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: 'OAuth not configured' });
   }
 
-  const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo&redirect_uri=${encodeURIComponent('https://yearbook.simpsonscarborough.com/api/callback')}`;
+  // Request both repo and read:org scopes to ensure org access
+  const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo,read:org&redirect_uri=${encodeURIComponent('https://yearbook.simpsonscarborough.com/api/callback')}`;
   
   res.redirect(authUrl);
 }
